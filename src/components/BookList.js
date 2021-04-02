a72789df95984fa8e00aa4f1f72f06997e4c40ea
chkrishnamurthy <chalapatikrishna@gmail.com>
2021-04-02 21:39:08 +0530

    initializedMERN

---
+++ src/components/BookList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BookItem from "./BookItem";
+
+const BookList = () => {
+  return (
+    <div>
+      <BookItem />
+    </div>
+  );
+};
+
+export default BookList;
